1a5433042452e34475e12966ac07fafe26e2c3c6
zaibakhan66 <129111102+zaibakhan66@users.noreply.github.com>
2023-05-14 22:09:27 -0700

    Add files via upload

---
+++ ch 3/app.js
@@ -0,0 +1,23 @@
+//===================chapter 3=====================
+
+//======================Q no 1=====================
+// let age =20
+// alert("I am " +  age  + " years old")
+
+// =====================Q no 2=============================
+
+// alert("you have visited this site 14 times")
+
+// =====================Q no 3=============================
+// let birth_year=2002;
+// document.write("my birth year is" + birth_year)
+// document.write("<br>")
+// document.write("Data tye of my declared variable is number")
+
+// =====================Q no 4=============================
+
+ let my_name = "zaiba khan";
+ let no_orders = 10;
+ let product_name = "pent";
+
+ document.write( "<b>" + my_name  + "</b>"+ " ordered " + "<b>"+ no_orders  + " "+ product_name +  "</b>(s)"+ " on XYZ clothing store");
