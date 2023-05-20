ec1dcb15bcab145bd4b1acad78f61c67293adaa7
zaibakhan66 <129111102+zaibakhan66@users.noreply.github.com>
2023-05-19 21:56:04 -0700

    Add files via upload

---
+++ CH 5/app.js
@@ -0,0 +1,123 @@
+//=========================== Q ON 1 =========================
+
+// let num_1 = +prompt("enter number 3");
+// let num_2 = +prompt("enter number 5");
+// let result = num_1 + num_2;
+// document.write("sum of " + num_1 + " and " + num_2 + " is " + result);
+
+//=========================== Q ON 2 =========================
+
+// let num1= +prompt("enter number 1")
+// let num2= +prompt("enter number 2")
+
+// document.write("sum of " , num1  ,  " and " , num2 , " is " , num1+num2 + "<br>")
+
+// document.write("subtraction of " , num1  ,  " and " , num2 , " is " , num1-num2 + "<br>")
+
+// document.write("multiplication of " , num1  ,  " and " , num2 , " is " , num1*num2 + "<br>")
+
+// document.write("division of " , num1  ,  " and " , num2 , " is " , num1%num2 + "<br>")
+
+//=========================== Q ON 3 =========================
+
+// let value ;
+// document.write("value after variable declaration is undefined" + "<br>")
+
+// document.write("initial value: 5" + "<br>")
+
+// document.write("value after increment is : 6" + "<br>")
+
+// document.write("value after addition is : 13" + "<br>")
+
+// document.write("value after decrement is : 12" + "<br>")
+
+// document.write("The remainder is : 0" + "<br>")
+
+//=========================== Q ON 4 =========================
+
+// let ticket_price = +prompt("enter price of one ticket");
+
+// let calculation =  +prompt("how many tickets u want to buy?");
+
+// document.write("total price of buying ", calculation, " ticket(s) is ", ticket_price * calculation)
+
+//=========================== Q ON 5 =========================
+
+// mytable() ;
+// function mytable(){
+//     var range=prompt("enter value: ");
+//     for (var i=1;i<=10;i++){
+//         var result = range*i;
+//         document.write(result+"<br>");
+//     }
+
+// }
+
+//=========================== Q ON 6 =========================
+
+// let F=prompt("enter temperature in Faherbeheit");
+// let C=(F-32)*5/9;
+// let Fa=(C*9/5)+32;
+// alert("Temperature in celsius ="+C);
+// document.write("Temperature in celsius C =" +C +"<br>");
+
+// alert("Temperature in celsius ="+Fa);
+// document.write("Temperature in celsius F=" +Fa);
+
+//=========================== Q ON 7 =========================
+
+//  let shopping_price_1 = 650;
+//  let quantity_1 = +prompt("enter quantity of item 1");
+//  let shopping_price_2 = 100;
+//  let quantity_2 = +prompt("enter quantity of item 2");
+//  let shipping_charges = 100;
+//  document.write("price of item 1 is: ",shopping_price_1, "<br>");
+//  document.write("price of item 2 is: ",shopping_price_2, "<br>");
+//  document.write("quantity of item 1 is: ",quantity_1, "<br>");
+//  document.write("quantity of item 2 is: ",quantity_2, "<br>");
+//  document.write("shipping price is: ",shipping_charges, "<br>");
+//  document.write("total cost is is: ",(shopping_price_1*quantity_1)+(shopping_price_2*quantity_2)
+//  +shipping_charges, "<br>");
+
+//=========================== Q ON 8 =========================
+
+// let mt=prompt("Enter the marks of maths out of 100");
+// let eg=prompt("Enter the marks of english out of 100");
+// let ur=prompt("Enter the marks of urdu out of 100");
+
+// let total=Number(mt)+Number(eg)+Number(ur)
+// let avg=Number(total)/3;
+// let pc=Number(total)*100/500;
+// document.write("Total marks of subject="+total);
+// document.write("<br> Average="+avg);
+// document.write("<br> Percentage"+pc);
+
+//=========================== Q ON 9 =========================
+
+// let dollar = +prompt("enter how many dollars you want to convert");
+// let riyal = +prompt("enter how many riyals you want to convert");
+// let dollar_1 = 108.80;
+// let riyal_3= 28;
+// document.write("<h1>Currency in PKR</h1> <br>")
+// document.write("total currency in PKR",(dollar_1*10)+ (riyal_3*25));
+
+//=========================== Q ON 11 =========================
+
+// let current_year=+prompt("Enter your current year");
+// let birth_year=+prompt("enter your birth year");
+// let age=current_year - birth_year
+
+// document.write("Current Year is:",current_year, "<br>");
+// document.write("Birth Year is:",birth_year,"<br>");
+// document.write("Age is:",age);
+
+//=========================== Q ON 12 =========================
+
+// let p,r,a;
+// p=3.1;
+// r=+prompt("Please Enter Radius of Circle");
+// a=p*r*r;
+// document.write("This is the area of circle="+a);
+
+
+
