8fdc9b9e43a8dfef8e09ccfbe0556e5586c34c4a
zaibakhan66 <129111102+zaibakhan66@users.noreply.github.com>
2023-05-22 10:00:00 -0700

    Add files via upload

---
+++ CH 9,11/app.js
@@ -0,0 +1,108 @@
+// =======================Q no 1=============================
+
+// var city=prompt('Enter your city name');
+// if(city==='Karachi'||city==='karachi'){
+//     alert('Welcome to city of lights')
+// }
+
+// =======================Q no 2=============================
+
+// let gender=prompt("enter your gender");
+// if(gender==='female'||gender==='Female'){
+//     alert('Good morning Maam')
+// }
+// else if(gender==='male'||gender==='Male'){
+//     alert('Good Morning Sir')
+// }
+
+// =======================Q no 3=============================
+
+// var color=prompt('Enter the traffic color');
+// if(color==='red'||color==='Red'){
+//     alert('Must stop')
+// }
+// else if(color==='yellow'||color==='Yellow'){
+//     alert('Ready to move')
+// }
+// else if(color==='green'||color==='Green'){
+//     alert('Move now')
+// }
+
+// =======================Q no 4=============================
+
+// var fuel = +prompt("Enter your remaining fuel in car in litres: ");
+// if(fuel<0.25){
+//     alert('Please refill the fuel in your car')
+// }
+
+// =======================Q no 5=============================
+     
+// var obtained_Marks=+prompt('Enter Obtained Marks in three subjects: ');
+// var total_Marks=+prompt('Enter total Marks:');
+// var percentage=(obtained_Marks/total_Marks)*100;
+// percentage=percentage.toFixed(2);
+
+// document.write('<h3>'+'Marksheet'+'</h3>');
+// document.write('Total marks :'+total_Marks+'<br>');
+// document.write('Percentage :'+percentage+'<br>');
+
+// var grade;
+// var remarks;
+// if(percentage>=80){
+//     grade="A-one";
+//     remarks="Excellent";
+// }
+// else if(percentage>=70){
+//     grade="A";
+//     remarks="Good";
+// }
+// else if(percentage>=60){
+//     grade="B";
+//     remarks="You need to improve";
+// }
+
+// else if(percentage<60){
+//     grade="Fail";
+//     remarks="Sorry";
+// }
+// else{
+//     document.write("Invalid Input" + "<br>")
+// }
+// document.write('Grade :'+grade+'<br>');
+// document.write('Remarks :'+remarks+'<br>');
+
+// =====================Q  no 8==================================
+
+// var num=+prompt('Enter number to check number is divisible by 3 or not');
+// if(num % 3===0){
+//     alert(num+' is divisible by 3')
+// }
+// else{
+//     alert(num+' is not  divisible by 3')
+// }
+
+// =====================Q  no 9==================================
+
+// var num=+prompt('Enter your weather');
+// if(num % 2===0){
+//     alert(num+' is even')
+// }
+// else{
+//     alert(num+' is odd')
+// }
+
+// =====================Q  no 10==================================
+
+// let t =prompt("enter your city temperature")
+// if ("T > 40 "){
+//     alert("“It is too hot outside.”")
+// }
+// else if ("T > 30 "){
+//     alert("“The Weather today is Normal.”")
+// }
+// else if ("T > 20 "){
+//     alert("“Today’s Weather is cool.”")
+// }
+// else if ("T > 10 "){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
