d7775acf91b7be2dc70af4c566b5ff8cf8b799d2
zaibakhan66 <129111102+zaibakhan66@users.noreply.github.com>
2023-06-05 12:43:49 -0700

    Add files via upload

---
+++ CH 15-17/app.js
@@ -0,0 +1,63 @@
+// ==================Q no 1===================
+
+// Declare an empty array using JS literal notation to store
+// student names in future.
+
+// let Array=[];
+
+// ==================Q no 2===================
+
+// Declare an empty array using JS object notation to store
+// student names in future
+
+// let array_1 = [];
+
+// ==================Q no 3===================
+
+// Declare and initialize a strings array.
+
+// let array = ["zaiba" "Sadia" "afshan"];
+
+// ==================Q no 4===================
+
+//  Declare and initialize a numbers array.
+
+// let Array = [13 , 25 , 39];
+// ==================Q no 5===================
+
+// Declare and initialize a boolean array.
+
+// let Array = [true , false];
+
+// ==================Q no 6===================
+
+//  Declare and initialize a mixed array
+
+// let Array = ["zaiba" "Sadia" "afshan", 13 , 25 , 39 ,true , false];
+
+// ==================Q no 7===================
+
+// Declare and Initialize an array and store available 
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS, 
+// BS, BCOM, MS, M. Phil., PhD). Show the listed 
+// qualifications in your browser like
+
+// let quali_arr = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil ', 'PhD'];
+// document.write("<h1> Qualification </h1>")
+// for(var i=0; i<quali_arr.length; i++){
+// document.write(quali_arr[i], "<br>")
+// }
+// ==================Q no 8===================
+
+// Write a program to store 3 student names in an array.Take
+// another array to store score of these three students. 
+// Assume that total marks are 500 for each student, display 
+// the scores & percentages of students like:
+
+let name_student = ["Michael" "John" "Tony"]
+let scored_marks = [320 ,230, 480]
+
+document.write("Score of michael is" ,name_student[0] , scored_marks[0] ,"Percentage:" , scored_marks[0]*100/500 );
+// ==================Q no 9===================
+// ==================Q no 10===================
+// ==================Q no 11===================
