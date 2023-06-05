c62fa7028e25145f0f59096349d9c1e1282c00d5
zaibakhan66 <129111102+zaibakhan66@users.noreply.github.com>
2023-06-05 12:43:13 -0700

    Add files via upload

---
+++ CH 12-13/app.js
@@ -0,0 +1,91 @@
+// =====================Q no 5==========================
+
+// let password='jeon jungkook'; 
+// let pass_1=prompt("enter your password");
+// if(pass_1===''){
+//     alert("enter your password ")
+// }
+// else if(pass_1===password){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Incorrect password")
+// }
+
+// =====================Q no 3==========================
+
+// let num=prompt("enter your weather number check are positive ,nagetive or zero ")
+
+// if(num>0){
+//     alert(num + " is positive")
+// }
+// else if(num<0){
+//     alert(num + " is nagetive")
+// }
+// else{
+//     alert(num + " is zero")
+// }
+
+
+
+
+
+
+
+
+// block scope
+// if (true){
+//     var i = 10;
+//     document.write(i + "<br>")
+// }
+
+
+
+
+
+
+
+
+// {
+//     let a =8;
+// }
+// console.log(a)
+
+// {
+//     var a =8;
+// }
+// console.log(a)
+
+// var foo = 'yo';
+// {
+//   var foo = 'hey';
+// }
+
+// console.log(foo); // Logs 'hey'
+
+
+let foo = 'yo';
+{
+  let foo = 'hey';
+}
+
+console.log(foo); // Logs 'yo'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
